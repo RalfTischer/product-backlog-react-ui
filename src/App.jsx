798dc67147dad63a1f74b6cd2280b0ddf344b703
Ralf <108927724+RalfTischer@users.noreply.github.com>
2024-02-28 21:30:19 +0100

    Add useEffects()

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,25 @@ import TaskTable from './components/TaskTable';
 import TaskApi from "./models/bridge.js";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  
+  useEffect(() => {
+  // Fetch tasks from the database when the component mounts
+  const db = new TaskAPI();
+  const fetchTasks = async () => {
+    try {
+      const tasksFromDB = await db.getAllTasks();
+      setTasks(tasksFromDB); // Set the fetched tasks to the state
+      console.log("### App #### tasksFromDB:", tasksFromDB);
+    } catch (error) {
+      // Handle error
+      console.error('Error fetching tasks: ', error);
+    }
+  };
+
+  fetchTasks();
+}, []);
+  
   // TODO: Read tasks
   const db = TaskAPI();
   const {setEditedTasks, editedTasks} = useState(db.getAllTasks());
