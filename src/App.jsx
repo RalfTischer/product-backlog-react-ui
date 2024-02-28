49081b70e43ad74b14f813b480a0fbd30f6e5845
Ralf <108927724+RalfTischer@users.noreply.github.com>
2024-02-28 15:44:32 +0100

    Add tasks, editedTasks and handleUpdate()

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import TaskTable from './components/TaskTable';
+import TaskApi from "./models/bridge.js";
 
 function App() {
   // TODO: Read tasks
-  const tasks = [];
+  const tasks = TaskAPI();
+  const {setEditedTasks, editedTasks} = useState(getAllTasks());
+  
+  const handleUpdate = (updatedTask) => {
+    console.log("handleUpdate", updatedTask);
+    // tasks.updateTask(updatedTask.id, updatedTask)
+  };
+  
   return (
     <div>
       <h1>Product Backlog</h1>
-      <TaskTable tasks={ tasks }/>
+      <TaskTable tasks={editedTasks}/>
     </div>
   );
 }
