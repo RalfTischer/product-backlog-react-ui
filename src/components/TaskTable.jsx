6e4a82b53b17077a984a1cfa63c03b446e837488
RalfTischer <ralf.tischer.1969@gmail.com>
2024-03-01 11:19:35 +0100

    Update css for table rows instead of style classes

--- src/components/TaskTable.jsx
+++ src/components/TaskTable.jsx
@@ -1,88 +1,90 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Task from './Task';
 
+// Hold: editedTask, editId
 
 const tableStyle = {
   width: '100%',
   tableLayout: 'fixed'
 }
   
-const tableColSmall = {
-  width: '10%'
-}
-  
-const tableColMedium = {
-  width: '20%'
-}
-  
-const tableColBig = {
-  width: '30%'
-}
-
 const TaskTable = ({  tasks, 
-                    handleCreate,
-                    handleUpdate,
-                    handleDelete,
-                    handleMove }) => {
+                      handleCreate,
+                      handleUpdate,
+                      handleDelete,
+                      handleMove }) => {
 
   const [editId, setEditId] = useState(null);
-  const [editedTasks, setEditedTasks] = useState(tasks);
-    
+  const [editedTasks, setEditedTasks] = useState([]);
+                      
+  useEffect(() => {
+    setEditedTasks([...tasks]); // Initialize editedTasks with a deep copy of tasks
+  }, [tasks]);
+  
   const handleEdit = (myTask) => {
+    //id = myTask.id;
+    // Cancel current edit
+    if (editId) {
+      const index  = editedTasks.findIndex(obj => obj.id === myTask.id);  // Index of task in edit mode
+      handleCancelEdit(editedTasks[index])
+    }
+
     setEditId(myTask.id);
   };
   
+  const handleCancelEdit = (myTask) => {
+    // TODO 
+    setEditedTasks(tasks);
+    setEditId(null);  
+  };
   const onCreate = (myTask) => {
-    const newTasks = editedTasks.push({pos: 0});
+    const newTasks = editedTasks.push({id: 0});   // New task with temporary id = 0
     setEditId(0);
     // TODO:Test 
   };
 
-  const onCancelCreateTask = () => {
-    // TODO 
-  };
 
-    return (
-      <div className="table-responsive-sm">
-        <table style={tableStyle} className="table table-dark table-hover w-100">
-            <colgroup>
-            <col span="1" style={tableColSmall} />
-            <col span="1" style={tableColSmall} />
-            <col span="1" style={tableColSmall} />
-            <col span="1" style={tableColBig} />
-            <col span="1" style={tableColSmall} />
-            <col span="1" style={tableColSmall} />
-            <col span="1" style={tableColMedium} />
-            </colgroup>
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Pos</th>
-                <th>Prio</th>
-                <th>Task</th>
-                <th>Time</th>
-                <th>Status</th>
-                <th>
-                Actions <button className="btn btn-sm btn-secondary" >*</button>
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            {editedTasks.push({pos.map(myTask => (
-                <Task 
-                  myTask={myTask} 
-                  editable={editId}
-                  handleEdit={handleEdit}
-                  handleCreate={handleCreate}
-                  handleUpdate={handleUpdate}
-                  handleDelete={handleDelete}
-                  handleMove={handleMove}
-                />
-            ))}
-            </tbody>
-        </table>
-        </div>
-    )
+  return (
+    <div className="table-responsive-sm">
+      <table style={tableStyle} className="table table-dark table-hover w-100">
+          <colgroup>
+          <col className="col-small" />
+          <col className="col-small" />
+          <col className="col-small" />
+          <col className="col-big" />
+          <col className="col-small" />
+          <col className="col-small" />
+          <col className="col-medium" />
+          </colgroup>
+          <thead>
+          <tr>
+              <th>ID</th>
+              <th>Pos</th>
+              <th>Prio</th>
+              <th>Task</th>
+              <th>Time</th>
+              <th>Status</th>
+              <th>
+              Actions <button className="btn btn-sm btn-secondary" >*</button>
+              </th>
+          </tr>
+          </thead>
+          <tbody>
+          {editedTasks.map(myTask => (
+              <Task 
+                myTask={myTask} 
+                editable={editId}
+                handleEdit={handleEdit}
+                handleCreate={handleCreate}
+                handleUpdate={handleUpdate}
+                handleDelete={handleDelete}
+                handleMove={handleMove}
+              />
+          ))}
+          </tbody>
+      </table>
+      </div>
+  )
 };
 
 export default TaskTable;
