5e528127f2238dabaa4ba795b2feff664b8be5df
RalfTischer <ralf.tischer.1969@gmail.com>
2024-03-02 16:08:52 +0100

    Update handleMove() with functionality. MoveToTop not yet working.

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,34 @@ import React, { useState, useEffect } from 'react';
 import TaskTable from './components/TaskTable';
 import TaskAPI from "./models/TaskAPI.js";
 
-// Hold tasks and bridge to model
+// Hold `tasks` 
+// Bridge to model `TaskAPI`
+
+let db; 
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  let db; 
-
+  
   useEffect(() => {
   // Fetch tasks from the database when the component mounts
   db = new TaskAPI();
-
   const fetchTasks = async () => {
     try {
-      const tasksFromDB = await db.getAllTasks();
+      let tasksFromDB = await db.getAllTasks("pos");
+
+      // Make sure to have sequential pos numbers
+      tasksFromDB.sort((a, b) => a.pos - b.pos).forEach((task, index) => {
+        task.pos = index + 1;
+      });
+
       setTasks(tasksFromDB); // Set the fetched tasks to the state
-      console.log("### App #### tasksFromDB:", tasksFromDB);
+      // console.log("### App #### Processed tasksFromDB:", tasksFromDB);
+  
     } catch (error) {
       // Handle error
       console.error('Error fetching tasks: ', error);
     }
+
   };
 
   fetchTasks();
@@ -45,6 +54,7 @@ function App() {
   const handleUpdate = (myTask) => {
     db = new TaskAPI();
     console.log("handleUpdate", myTask);
+
     const updatedTasks = [...tasks];
     const taskIndex = updatedTasks.findIndex(task => task.id === myTask.id);
     if (taskIndex !== -1) {
@@ -58,6 +68,7 @@ function App() {
     if (window.confirm("Really delete task " + myTask.id + " [" + myTask.task +"]?")) {
       db = new TaskAPI();
       console.log("deleteCreate", myTask.id);
+
       let updatedTasks = [...tasks];
       updatedTasks = updatedTasks.filter(task => task.id !== myTask.id);
       db.deleteTask(myTask.id);
@@ -67,6 +78,30 @@ function App() {
 
   const handleMove = (myTask, positions) => {
     console.log("handleMove", myTask, "by positions", positions);
+
+    let updatedTasks = [...tasks];
+    const currentIndex = updatedTasks.findIndex(task => task.id === myTask.id);
+    if (currentIndex !== -1) {
+      const newPos = updatedTasks[currentIndex].pos + positions;
+      if ((positions === 0) || (newPos <= 0)) {
+          updatedTasks[currentIndex].pos = 1;
+      } else if (newPos > updatedTasks.length) {
+          updatedTasks[currentIndex].pos = updatedTasks.length;
+      } else {
+          updatedTasks.splice(currentIndex, 1);
+          updatedTasks.splice(newPos - 1, 0, myTask);
+      }
+      console.log("updatedTasks before db", updatedTasks);
+
+      // Update database
+      db = new TaskAPI();
+      updatedTasks.forEach(async (task, index) => {
+        task.pos = index + 1;
+        await db.updateTask(task.id, task); // Update each task in the database
+      });
+        console.log("updatedTasks after db", updatedTasks);
+        setTasks(updatedTasks);
+    }
   }
     
   return (
