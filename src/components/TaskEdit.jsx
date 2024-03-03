dc700bc362bde55907478659cbb32401b2c10c58
RalfTischer <ralf.tischer.1969@gmail.com>
2024-03-03 12:51:16 +0100

    Update format

--- src/components/TaskEdit.jsx
+++ src/components/TaskEdit.jsx
@@ -32,16 +32,16 @@ const TaskEdit = ({ myKey,
   return (
     <>
       <tr key={myKey} >
-      <td>{editedTask.id}</td>
-      <td><input name="pos" style={inputStyle} type="text" value={editedTask.pos} onChange={handleInputChange} /></td>
-      <td><input name="prio" style={inputStyle} type="text" value={editedTask.prio} onChange={handleInputChange} /></td>
-      <td><input name="task" style={inputStyle} type="text" value={editedTask.task} onChange={handleInputChange} /></td>
-      <td><input name="time" style={inputStyle} type="text" value={editedTask.time} onChange={handleInputChange} /></td>
-      <td><input name="status" style={inputStyle} type="text" value={editedTask.status} onChange={handleInputChange} /></td>
-      <td>
-          <button onClick={onSave}>Save</button>
-          <button onClick={onCancel}>Cancel</button>
-      </td>
+        <td>{editedTask.id}</td>
+        <td><input name="pos" style={inputStyle} type="text" value={editedTask.pos} onChange={handleInputChange} /></td>
+        <td><input name="prio" style={inputStyle} type="text" value={editedTask.prio} onChange={handleInputChange} /></td>
+        <td><input name="task" style={inputStyle} type="text" value={editedTask.task} onChange={handleInputChange} /></td>
+        <td><input name="time" style={inputStyle} type="text" value={editedTask.time} onChange={handleInputChange} /></td>
+        <td><input name="status" style={inputStyle} type="text" value={editedTask.status} onChange={handleInputChange} /></td>
+        <td>
+            <button onClick={onSave}>Save</button>
+            <button onClick={onCancel}>Cancel</button>
+        </td>
       </tr>
     </>
   ); 
