40b006bb5b822891e5d01aed1c1c6cd4f93b6f2d
RalfTischer <ralf.tischer.1969@gmail.com>
2024-03-03 12:49:40 +0100

    Fix MoveToTop, delete logs

--- src/App.jsx
+++ src/App.jsx
@@ -31,29 +31,25 @@ function App() {
     }
 
   };
-
   fetchTasks();
   }, []);
   
-  
   const handleCreate = async (myTask) => {
+    // Create new task
     db = new TaskAPI();
     const updatedTasks = [...tasks];  // Create a copy of tasks
     const newId = await db.createTask(myTask);  // Save and get new id
-    console.log("new id:", newId);
 
     let newTask ={ ... myTask};       // Create a copy of the new task
     newTask.id = newId;               // Set new id
-    console.log("newTask", newTask);
     
     updatedTasks.push(newTask);
     setTasks(updatedTasks);
-    console.log("updated tasks", updatedTasks);
   };
 
   const handleUpdate = (myTask) => {
+    // Update task with new data
     db = new TaskAPI();
-    console.log("handleUpdate", myTask);
 
     const updatedTasks = [...tasks];
     const taskIndex = updatedTasks.findIndex(task => task.id === myTask.id);
@@ -65,9 +61,9 @@ function App() {
   }
   
   const handleDelete = (myTask) => {
+    // Delete task
     if (window.confirm("Really delete task " + myTask.id + " [" + myTask.task +"]?")) {
       db = new TaskAPI();
-      console.log("deleteCreate", myTask.id);
 
       let updatedTasks = [...tasks];
       updatedTasks = updatedTasks.filter(task => task.id !== myTask.id);
@@ -77,35 +73,29 @@ function App() {
   };
 
   const handleMove = (myTask, positions) => {
-    console.log("handleMove", myTask, "by positions", positions);
-
-    let updatedTasks = [...tasks];
-
+    // Move position of task
     // Shift position of myTask in array
+    let updatedTasks = [...tasks];
     const currentIndex = updatedTasks.findIndex(task => task.id === myTask.id);
+    
     if (currentIndex !== -1) {
-      if (positions === 0) {
-        const newPos = 0;   // Set newTask to first position 
-      } else {
-        const newPos = updatedTasks[currentIndex].pos + positions;
-      } 
+      const newPos = updatedTasks[currentIndex].pos + positions;
       if (newPos <= 0) {
           updatedTasks[currentIndex].pos = 1;
+          updatedTasks.splice(currentIndex, 1);
+          updatedTasks.splice(newPos - 1, 0, myTask);
       } else if (newPos > updatedTasks.length) {
           updatedTasks[currentIndex].pos = updatedTasks.length;
       } else {
           updatedTasks.splice(currentIndex, 1);
           updatedTasks.splice(newPos - 1, 0, myTask);
       }
-      console.log("updatedTasks before db", updatedTasks);
-
       // Update database
       db = new TaskAPI();
       updatedTasks.forEach(async (task, index) => {
         task.pos = index + 1;
         await db.updateTask(task.id, task); // Update each task in the database
       });
-        console.log("updatedTasks after db", updatedTasks);
         setTasks(updatedTasks);
     }
   }
